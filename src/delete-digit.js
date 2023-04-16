59f495107c2adab4e97882f1f60af068709feda5
Dmitry Andriyanov <facklak@mail.ru>
2023-04-16 16:14:17 +0300

    feat: add delete-digin tospliced is not function

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let arr = Array.from(String(n)),
+      count,
+      summ = arr.toSpliced(0,1);
+  for (let i=0;i<=arr.length-1;i++) {
+    let numberSumm = Number(summ.toString().replace(/[\,]/g,'')),
+        newSumm = Number(arr.toSpliced(i,1).toString().replace(/[\,]/g,''));
+    if (numberSumm < newSumm) {
+      count = newSumm;
+      summ = arr.toSpliced(i,1);
+    } else {
+      count = numberSumm;
+    }
+  }
+  return count
 }
 
 module.exports = {
