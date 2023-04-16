d04a33f05af6a90bc97e4d4d35dd88eaecabc71c
Dmitry Andriyanov <facklak@mail.ru>
2023-04-16 12:34:16 +0300

    feat: add common-character

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let count = 0,
+  text1 = s1.split(''),
+  text2 = s2.split('');
+  function findRecurse(str1, str2) {
+    if (str1.length > 0 && str2.length > 0) {
+      if (str1.length >= str2.length) {
+        for (let i=0;i<=str1.length-1;i++) {
+          for (let a=0;a<=str2.length-1;a++) {
+            if (str1[i] === str2[a]) {
+              count++
+              text1.shift();
+              text2.splice(a,1);
+              findRecurse(text1, text2)
+            }
+          }
+        }
+      } else {
+        for (let i=0;i<=str1.length-1;i++) {
+          for (let a=0;a<=str2.length-1;a++) {
+            if (str1[i] === str2[a]) {
+              count++
+              text1.shift();
+              text2.splice(a,1);
+              findRecurse(text1, text2)
+            }
+          }
+        }
+      }
+    }
+    return
+  }
+  findRecurse(text1, text2)
+  return count;
 }
 
 module.exports = {
